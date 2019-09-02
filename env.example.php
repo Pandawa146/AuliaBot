9597493d320d5dd6f7804b11213d5b14045f6c17
Pandawa146 <54611218+Pandawa146@users.noreply.github.com>
2019-09-02 22:57:24 +0700

    Add files via upload

---
+++ env.example.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Misha Serenkov
+ * Email: mi.serenkov@gmail.com
+ * Date: 15.10.2017 23:02
+ */
+
+return [
+    'TELEGRAM_BOT_TOKEN' => '925651312:AAHT0AT_pZWxplc0Vqi52e7ldKA09f_EHIM',
+];
